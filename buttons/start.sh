700d1e56e587abc2e496b37aba03262049c3a083
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2025-03-04 17:31:47 -0500

    feat: update scripts

--- buttons/start.sh
+++ buttons/start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-sudo systemctl daemon-reload
 sudo systemctl restart button-monitor.service
--- gyro/restart.sh
+++ gyro/restart.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-sudo systemctl daemon-reload
 sudo systemctl restart gyro-monitor.service
