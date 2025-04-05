d452614739750721b20b244db6c540f1a64134a3
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2025-04-05 01:48:24 -0400

    feat: everything

--- camera/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-h="192.168.8.220"
-echo http://$h:8005/video_stream
-sudo python3 camera_stream.py -m yolov8ntrained_openvino_2022.1_6shave.blob -c yolov8ntrained.json
--- camera/status.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo journalctl -u camera-stream.service
